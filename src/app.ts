2798eaa7010d0e55037e6ad5a11595e043c24618
Abir <malam2331103@bscse.uiu.ac.bd>
2026-07-01 20:32:04 +0600

    cors apply

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import express, { type Application, type Request, type Response } from "express"
+import cors from "cors"
 import { userRouter } from "./modules/users/user.route";
 import logger from "./middleware/logger";
 import { authRouter } from "./modules/auth/auth.route";
 import { issuesRouter } from "./modules/issues/issues.route";
-const app:Application = express()
+const app: Application = express()
 
 app.use(express.json());
 app.use(logger);
+const corsOptions = {
+  origin: 'http://localhost:3000',
+}
+app.use(cors(corsOptions));
 
-app.use('/api/auth',userRouter)
+app.use('/api/auth', userRouter)
 app.use('/api/auth', authRouter)
-app.use("/api/issues",issuesRouter)
-app.get('/', (req:Request, res:Response) => {
+app.use("/api/issues", issuesRouter)
+app.get('/', (req: Request, res: Response) => {
   res.send('Server is running!!!')
 })
 
